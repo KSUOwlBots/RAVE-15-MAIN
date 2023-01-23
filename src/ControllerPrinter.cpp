94dd3b1323a75f3739d3c0ff1ddc9e005deaf7da
ZaidKhan <zaidkhan5213@gmail.com>
2023-01-23 18:50:26 -0500

    controllername

--- src/ControllerPrinter.cpp
+++ src/ControllerPrinter.cpp
@@ -10,16 +10,17 @@
 void PrintInfo(void *) {
 
   pros::Imu imu_sensor(10);
-  pros::Controller master (pros::E_CONTROLLER_MASTER);
+  // pros::Controller controller1 (pros::E_CONTROLLER_MASTER);
   int count = 0;
 
     while (true) {
 
       if(!(count % 25)) {
+        
 
-        master.print(0, 0, "X-position of IMU: %f degrees\n", imu_sensor.get_heading());
+        controller1.print(0, 0, "X-position of IMU: %f degrees\n", imu_sensor.get_heading());
 
-        master.print(0, 0, "Z-position of IMU: %f degress\n", imu_sensor.get_rotation());
+        controller1.print(0, 0, "Z-position of IMU: %f degress\n", imu_sensor.get_rotation());
 
       }
 
